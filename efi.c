b90bda3998b4cb29cd1b96c4084837c2cc60563d
Sovereign Shahid <sovereignshahid@gmail.com>
2024-01-07 15:02:05 -0700

    made game loop work

--- efi.c
+++ efi.c
@@ -5,12 +5,6 @@
 typedef enum {
   UP,DOWN,LEFT,RIGHT
 } DIRECTION;
-typedef struct Ctx {
-  EFI_SYSTEM_TABLE* ST;
-  UINT16* snake_size;
-  BOOLEAN* grow;
-  DIRECTION* dir;
-} Ctx;
 typedef struct Vec2 {
   INTN X;
   INTN Y;
@@ -20,14 +14,15 @@ Vec2 snake[COL-2*ROW-2] = { { .X=-1, .Y=-1 } };
 UINT16 snake_size = 0;
 BOOLEAN grow = 0;
 DIRECTION dir = RIGHT;
-EFI_EVENT game_loop_event;
+UINTN idx = 0;
+EFI_EVENT events[2];
 
 VOID init_board() {
   // Initialize I/O
   ST->ConOut->SetMode(ST->ConOut, 0); // Screen is 80x25 characters
   ST->ConOut->Reset(ST->ConOut, 1);
   ST->ConIn->Reset(ST->ConIn, 1);
-  ST->ConOut->EnableCursor(ST->ConOut, 1);
+  ST->ConOut->EnableCursor(ST->ConOut, 0);
   // Draw borders
   for(UINT8 x=0; x<COL; x++) {
     for(UINT8 y=0; y<ROW; y++) {
@@ -55,8 +50,6 @@ VOID move_snake(INT8 dx, INT8 dy) {
 
 VOID read_keyboard() {
   EFI_KEY_DATA keyData;
-  UINTN idx;
-  ST->BootServices->WaitForEvent(1, &ST->ConIn->WaitForKeyEx, &idx);
   ST->ConIn->ReadKeyStrokeEx(ST->ConIn, &keyData);
   if (keyData.Key.UnicodeChar == u'w') {
     if (dir != DOWN)
@@ -96,7 +89,7 @@ BOOLEAN snake_on_self() {
 }
 
 VOID game_over() {
-  ST->BootServices->CloseEvent(game_loop_event);
+  ST->BootServices->CloseEvent(events[1]);
   while(1) {
     ST->ConOut->ClearScreen(ST->ConOut);
     ST->ConOut->SetAttribute(ST->ConOut, EFI_RED);
@@ -107,48 +100,39 @@ VOID game_over() {
   }
 }
 
-VOID EFIAPI game_loop(EFI_EVENT Event, VOID* Context) {
-  (void)Event;
-  Ctx* ctx = Context;
-  EFI_SYSTEM_TABLE* ST = ctx->ST;
-  UINT16* snake_size = ctx->snake_size;
-  BOOLEAN* grow = ctx->grow;
-  DIRECTION* dir = ctx->dir;
-  ST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, 0);
-  /* ignore this for now
-  if (*grow) {
-      *snake_size+=1;
+VOID game_loop() {
+  if (grow) {
+      snake_size+=1;
     }
-    if (*dir==UP) {
-      if (!*grow) {
-        ST->ConOut->SetCursorPosition(ST->ConOut, snake[*snake_size-1].X, snake[*snake_size-1].Y);
+    if (dir==UP) {
+      if (!grow) {
+        ST->ConOut->SetCursorPosition(ST->ConOut, snake[snake_size-1].X, snake[snake_size-1].Y);
         ST->ConOut->OutputString(ST->ConOut, u" ");
       }
       move_snake(0,-1);
     }
-    if (*dir==DOWN) {
-      if (!*grow) {
-        ST->ConOut->SetCursorPosition(ST->ConOut, snake[*snake_size-1].X, snake[*snake_size-1].Y);
+    if (dir==DOWN) {
+      if (!grow) {
+        ST->ConOut->SetCursorPosition(ST->ConOut, snake[snake_size-1].X, snake[snake_size-1].Y);
         ST->ConOut->OutputString(ST->ConOut, u" ");
       }
       move_snake(0,1);
     }
-    if (*dir==LEFT) {
-      if (!*grow) {
-        ST->ConOut->SetCursorPosition(ST->ConOut, snake[*snake_size-1].X, snake[*snake_size-1].Y);
+    if (dir==LEFT) {
+      if (!grow) {
+        ST->ConOut->SetCursorPosition(ST->ConOut, snake[snake_size-1].X, snake[snake_size-1].Y);
         ST->ConOut->OutputString(ST->ConOut, u" ");
       }
       move_snake(-1,0);
     }
-    if (*dir==RIGHT) {
-      if (!*grow) {
-        ST->ConOut->SetCursorPosition(ST->ConOut, snake[*snake_size-1].X, snake[*snake_size-1].Y);
+    if (dir==RIGHT) {
+      if (!grow) {
+        ST->ConOut->SetCursorPosition(ST->ConOut, snake[snake_size-1].X, snake[snake_size-1].Y);
         ST->ConOut->OutputString(ST->ConOut, u" ");
       }
       move_snake(1,0);
     }
-    *grow = 0;
-    */
+    grow = 0;
 }
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
@@ -158,21 +142,25 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 
   init_board();
   //Snake pos can be within (1,1) and (78,22)
-  Ctx ctx = {
-    .ST = ST,
-    .snake_size = &snake_size,
-    .grow = &grow,
-    .dir = &dir,
-  };
   // Create event
-  ST->BootServices->CreateEvent(EVT_TIMER, TPL_APPLICATION, game_loop, &ctx, game_loop_event);
-  ST->BootServices->SetTimer(game_loop_event, TimerRelative, 5);
+  events[0] = ST->ConIn->WaitForKeyEx;
+  ST->BootServices->CreateEvent(EVT_TIMER, TPL_CALLBACK, (EFI_EVENT_NOTIFY)0, (VOID*)0, &events[1]);
+  ST->BootServices->SetTimer(events[1], TimerPeriodic, 5e5);
   while(1) {
     if (snake[0].X <=0 || snake[0].X >= COL-1 || snake[0].Y <= 0 || snake[0].Y >= ROW-1) {
       game_over();
     } 
     draw_snake();
     read_keyboard();
+    EFI_STATUS Status = ST->BootServices->WaitForEvent(2, events, &idx);
+    if (EFI_SUCCESS == Status) {
+      if(idx == 0) {
+        read_keyboard();
+      }
+      else if (idx == 1){
+        game_loop();
+      }
+    }
     if (snake_on_self()) {
       game_over();
     }
